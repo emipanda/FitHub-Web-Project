c4f6da15d4e668b82ca3442d95e70686f6a3198c
Emipanda <eparkour@gmail.com>
2021-10-16 12:23:16 +0300

    first commit

---
+++ FitnessStore/Data/FitnessStoreContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FitnessStore.Models;
+
+namespace FitnessStore.Data
+{
+    public class FitnessStoreContext : DbContext
+    {
+        public FitnessStoreContext (DbContextOptions<FitnessStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FitnessStore.Models.Product> Product { get; set; }
+
+        public DbSet<FitnessStore.Models.Supplier> Supplier { get; set; }
+
+        public DbSet<FitnessStore.Models.ShoppingCart> ShoppingCart { get; set; }
+
+        public DbSet<FitnessStore.Models.User> User { get; set; }
+    }
+}
---
+++ FitnessStore/Models/UserStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//a class to know what is te state of the user
+namespace WebApp2021.Models
+{
+    public enum UserState
+    {
+        NotLoggedIn,
+        RegularUser,
+        RegularUserForbidden,
+        Manager
+    }
+}
